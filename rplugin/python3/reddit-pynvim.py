fc0bbf856a92014ef4efa9f273d529998d330f00
ziontee113 <ziontee113@gmail.com>
2024-01-04 07:26:30 +0700

    Add notify method

--- rplugin/python3/reddit-pynvim.py
+++ rplugin/python3/reddit-pynvim.py
@@ -6,8 +6,19 @@ class RedditNvimPlugin(object):
     def __init__(self, nvim: pynvim.Nvim):
         self.nvim = nvim
 
-    @pynvim.command("RedditPynvim", nargs="1")
-    def copilotChat(self, args: list[str]):
-        buf = self.nvim.current.buffer
-        # self.nvim.notify("ok reddit.nvim", "info")
-        self.nvim.api.nvim_notify("This is a test notification", 1, {})
+    def notify(self, msg: any):
+        def format_msg(msg):
+            if type(msg) is str:
+                msg = msg.split("\n")
+                msg = str(msg)
+                msg = "{" + msg[1:-1] + "}"
+            else:
+                msg = f"'{msg}'"
+            return msg
+
+        formatted_msg = format_msg(msg)
+        self.nvim.exec_lua(f'vim.notify({formatted_msg}, "info")')
+
+    @pynvim.command("RedditPynvim")
+    def just_testing(self):
+        self.notify("Hello from Python!")
