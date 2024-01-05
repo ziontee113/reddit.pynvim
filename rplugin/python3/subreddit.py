a6925552b5698ab725781fa1cf1d889f0b9de204
ziontee113 <ziontee113@gmail.com>
2024-01-06 04:43:05 +0700

    Add fetch method to SubredditNvim class

--- rplugin/python3/subreddit.py
+++ rplugin/python3/subreddit.py
@@ -1,8 +1,9 @@
 from rplugin.python3.mypynvim.nvim import MyNvim
 from enum import Enum
+from praw import Reddit
 
 
-class SubredditTimeFilter(Enum):
+class TimeFilter(Enum):
     HOUR = "hour"
     DAY = "day"
     WEEK = "week"
@@ -11,7 +12,7 @@ class SubredditTimeFilter(Enum):
     ALL = "all"
 
 
-class SortSubredditBy(Enum):
+class SortBy(Enum):
     HOT = "hot"
     NEW = "new"
     TOP = "top"
@@ -22,16 +23,35 @@ class SortSubredditBy(Enum):
 class SubredditNvim:
     def __init__(
         self,
+        reddit: Reddit,
         nvim: MyNvim,
         subreddit_name: str,
         sort_by: str = "TOP",
         time_filter: str = "WEEK",
         limit: int = 25,
     ):
+        self.reddit = reddit
         self.nvim = nvim
-        self.subreddit_instance_info = {
+        self.parameters = {
             "name": subreddit_name,
-            "sort_by": SortSubredditBy[sort_by].value,
-            "time_filter": SubredditTimeFilter[time_filter].value,
+            "sort_by": SortBy[sort_by],
+            "time_filter": TimeFilter[time_filter],
             "limit": limit,
         }
+        self.fetched_posts = []
+
+    def fetch(self):
+        subreddit = self.reddit.subreddit(self.parameters["name"])
+        sort_methods = {
+            SortBy.HOT: subreddit.hot,
+            SortBy.NEW: subreddit.new,
+            SortBy.RISING: subreddit.rising,
+            SortBy.CONTROVERSIAL: subreddit.controversial,
+            SortBy.TOP: subreddit.top,
+        }
+        sort_args = {
+            "limit": self.parameters["limit"].get(),
+        }
+        if self.parameters["sort_by"] in [SortBy.TOP, SortBy.CONTROVERSIAL]:
+            sort_args["time_filter"] = self.parameters["time_filter"].get()
+        self.fetched_posts = sort_methods[self.parameters["sort_by"]](**sort_args)
